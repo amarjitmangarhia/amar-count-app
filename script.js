355f78cf9ccd8d2937f471b223a3a5659cb86122
Amarjit Singh <62531546+amarjitmangarhia@users.noreply.github.com>
2021-05-31 10:55:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const increase = document.querySelector(".increase");
+const decrease = document.querySelector(".decrease");
+const counter = document.querySelector(".counter");
+const msg = document.querySelector(".msg");
+const number = document.querySelector("#number");
+const btn = document.querySelector("#btn");
+
+let count = 0;
+
+const fadeOut = function () {
+  setTimeout(() => {
+    msg.style.opacity = 0;
+  }, 3000);
+};
+
+increase.addEventListener("click", function () {
+  if (msg) {
+    msg.style.opacity = 0;
+  }
+  count++;
+  counter.textContent = count;
+});
+
+decrease.addEventListener("click", function () {
+  if (count === 0) {
+    msg.style.opacity = 100;
+    fadeOut();
+    return;
+  }
+  count--;
+  counter.textContent = count;
+});
+
+btn.addEventListener("click", function () {
+  let x = Number(number.value);
+  if (x < 0) {
+    msg.style.opacity = 100;
+    fadeOut();
+  } else if (x > 9999999) {
+    msg.style.opacity = 100;
+    msg.textContent = "Can't go above 9999999";
+    fadeOut();
+  } else {
+    counter.textContent = count = count + x;
+  }
+});
+
+increase.addEventListener("mouseover", function () {
+  counter.style.border = "1px solid #f15223";
+});
+
+decrease.addEventListener("mouseover", function () {
+  counter.style.border = "1px solid #f15223";
+});
+
+increase.addEventListener("mouseout", function () {
+  counter.style.border = "1px solid #2d9e64";
+  console.log("ok");
+});
+
+decrease.addEventListener("mouseout", function () {
+  counter.style.border = "1px solid #2d9e64";
+});
